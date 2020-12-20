345d8a18080be62c4d2beec9030a61264ecec17c
Constantine <golovnev1306@mail.ru>
2020-12-21 00:03:07 +0800

    + typescript have been installing, eslincache was be added into gitignore

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import s from './Header.module.css';
 import {Link} from 'react-router-dom';
 
-const Header = (props) => {
+const Header = ({state:{isLogin, unlogin, userData}}) => {
     //debugger
     return (
         <header className={s.header}>here header
-            {props.state.isLogin && `, hello ${props.state.userData.login}`}
-            {props.state.isLogin && <button onClick={props.state.unlogin}>Unlogin</button>}
-            {!props.state.isLogin && <Link to='/login'><button style={{marginLeft: "20px"}}>login</button></Link>}
+            {isLogin && `, hello ${userData.login}`}
+            {isLogin && <button onClick={unlogin}>Unlogin</button>}
+            {!isLogin && <Link to='/login'><button style={{marginLeft: "20px"}}>login</button></Link>}
         </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -8,14 +8,11 @@ import store from "./redux/store";
 import {Provider} from 'react-redux';
 
 
-ReactDOM.render(
-    //<React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>,
-    document.getElementById('root')
+ReactDOM.render( <BrowserRouter>
+            <Provider store={store} >
+                <App />
+            </Provider >
+        </BrowserRouter>, document.getElementById('root')
     //</React.StrictMode>
 );
 
